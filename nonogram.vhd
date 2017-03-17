2dbf1ed6dcd18d2c73f0f7fc53d04d888d029ab0
danielefongo! <danielefongo@gmail.com>
2017-03-17 12:30:46 +0000

    Project cleared.

--- nonogram.vhd
+++ nonogram.vhd
@@ -1,42 +1,58 @@
 library IEEE;
- use IEEE.STD_LOGIC_1164.ALL;
- use IEEE.numeric_std.all;
- use work.vga_package.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
+use work.nonogram_package.all;
  
 entity nonogram is
+
    port
 	(
-		CLOCK_50            : in  std_logic;
-		KEY                 : in  std_logic_vector(3 downto 0);
-		LEDR					  : out std_logic_vector(17 downto 0);
-		LEDG					  : out std_logic_vector(7 downto 0);
+		CLOCK_50            	: in  std_logic;
 		
-		SW                  : in  std_logic_vector(17 downto 0);
-		VGA_R               : out std_logic_vector(7 downto 0);
-		VGA_G               : out std_logic_vector(7 downto 0);
-		VGA_B               : out std_logic_vector(7 downto 0);
-		VGA_HS              : out std_logic;
-		VGA_VS              : out std_logic;
+		SW                  	: in  std_logic_vector(17 downto 0);
 		
-		SRAM_ADDR           : out   std_logic_vector(19 downto 0);
-		SRAM_DQ             : inout std_logic_vector(15 downto 0);
-		SRAM_CE_N           : out   std_logic;
-		SRAM_OE_N           : out   std_logic;
-		SRAM_WE_N           : out   std_logic;
-		SRAM_UB_N           : out   std_logic;
-		SRAM_LB_N           : out   std_logic
+		KEY                 	: in  std_logic_vector(3 downto 0);
+				
+		VGA_R               	: out std_logic_vector(7 downto 0);
+		VGA_G               	: out std_logic_vector(7 downto 0);
+		VGA_B               	: out std_logic_vector(7 downto 0);
+		VGA_HS              	: out std_logic;
+		VGA_VS              	: out std_logic;
+		VGA_SYNC_N				: out std_logic;
+		VGA_BLANK_N				: out std_logic;
+		VGA_CLK					: out std_logic;
+		
+		HEX7						: out std_logic_vector(6 to 0);
+		HEX6						: out std_logic_vector(6 to 0);
+		HEX3						: out std_logic_vector(6 to 0);
+		HEX2						: out std_logic_vector(6 to 0);
+		HEX1						: out std_logic_vector(6 to 0);
+		HEX0						: out std_logic_vector(6 to 0);
+		
+		LERG						: out std_logic_vector(8 to 0);
+		LEDR						: out std_logic_vector(17 to 0)
 	);
+	
 end nonogram;
 
 architecture RTL of nonogram is
 
 	-- Signal declaration
-	signal clock		: std_logic;
-	signal vga_clock	: std_logic;
-	signal RESET_N		: std_logic;
+	signal clock					: std_logic;
+	signal vga_clock				: std_logic;
+	signal reset_n					: std_logic;
+	signal reset_sync				: std_logic;
+	signal level					: integer range 0 to MAX_LEVEL - 1;
+	signal status					: status_type;
+	signal ack						: status_type;
+	signal row_index				: integer range 0 to MAX_COLUMN - 1;
+	signal row_description		: line_type;
+	signal iteration				: integer range 0 to MAX_ITERATION - 1;
+	signal undefined_cells		: integer range 0 to MAX_ROW * MAX_COLUMN - 1;
 	
 begin
 	
+	--entities
 	pll: entity work.PLL
 		port map
 		(
@@ -44,6 +60,20 @@ begin
 				c0			=> clock,
 				c1			=> vga_clock
 		);
-
+	
+	-- processes
+	reset : process(clock)
+	begin
+		if(rising_edge(clock)) then
+			reset_sync <= SW(0);
+			reset_n <= reset_sync;
+		end if;
+	end process;
+	
+	vga_clock_forward : process(vga_clock)
+	begin
+		VGA_CLK <= vga_clock;
+	end process;
+		
 end architecture;
  
--- nonogram_package.vhd
+++ nonogram_package.vhd
@@ -1,176 +1,114 @@
 library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
-use work.vga_package.all;
 
 package nonogram_package is
-	constant COLUMNS					:	positive	:= 10;
-	constant ROWS						:	positive	:= 10;
-	constant BOARD_SIZE				:	positive	:= 10; -- TODO: implement a MAX function here!
-	constant CLUES_ROWS				:	positive	:=	5; -- ceil(ROWS / 2)
-	constant CLUES_COLUMNS			: 	positive	:=	5; -- ceil(COLUMNS / 2)
-	constant CLUES_SIZE				: 	positive := 5; -- TODO: implement a MAX function here!
 
-	-- cells
-	type cell	is (FULL, EMPTY, UNDEFINED);
-	type cell_position is record
-		row	: integer range 0 to (ROWS - 1);	
-		col	: integer range 0 to (COLUMNS - 1);
-	end record;
+	--CONSTANTS
+	constant MAX_ROW					: integer := 40;
+	constant MAX_COLUMN				: integer := 30;
+	constant MAX_LINE					: integer := 40; -- MAX(MAX_ROW, MAX_COLUMN)
 	
+	constant MAX_CLUE					: integer := 19;
+	constant MAX_CLUE_ROW			: integer := 20; -- CEIL(MAX_ROW / 2)
+	constant MAX_CLUE_COLUMN		: integer := 15; -- CEIL(MAX_COLUMN / 2)
+	constant MAX_CLUE_LINE			: integer := 20; -- CEIL(MAX_LINE / 2)
+	
+	constant MAX_LEVEL				: integer := 2;
+	constant MAX_ITERATION			: integer := 100;
+	
+	--TYPES
 	attribute enum_encoding	: string;
-	attribute enum_encoding of cell_position : type is "sequential"; -- sequential (binary) --> 00, 01, 10
 	
-	-- board
-	type board_array_type is array(natural range <>, natural range <>) of cell;
-	type board_type is record
-		cells	:	board_array_type(0 to (ROWS - 1), 0 to (COLUMNS - 1));
+	type status_type is (IDLE, LOAD, SOLVE_ITERATION, SOLVE_ALL, WON, LOST);
+	
+	--cells
+	type cell_type is (INVALID, UNDEFINED, EMPTY, FULL);
+	attribute enum_encoding of cell_type : type is "sequential";
+	
+	type cell_position_type is record
+		row	: integer range -1 to (MAX_ROW - 1); -- -1 for invalid cell position
+		col	: integer range -1 to (MAX_COLUMN - 1); -- -1 for invalid cell position
 	end record;
 	
-	-- clues
-	subtype clue is integer;
-	type clues_array_type is array(natural range <>, natural range <>) of integer range	0 to BOARD_SIZE; -- Be careful. Possible overflow here!!
-	type clues_board_type is record
-		rows	:	clues_array_type(0 to (ROWS - 1), 0 to (CLUES_COLUMNS - 1));
-		cols	: 	clues_array_type(0 to (COLUMNS - 1), 0 to (CLUES_ROWS - 1));
+	type cell_array_position_type is array(natural range <>) of cell_position_type;
+	
+	--lines
+	type line_type is array(0 to (MAX_LINE - 1)) of cell_type;
+	
+	--clues
+	subtype clue_type is integer range -1 to MAX_CLUE; -- -1 for invalid clue
+	type clue_matrix_type is array(natural range <>, natural range <>) of clue_type; 
+	
+	--levels
+	type level_type is record
+		rows				:	integer range 0 to MAX_COLUMN;
+		columns			:	integer range 0 to MAX_ROW;
+		clue_rows		:	clue_matrix_type(0 to MAX_COLUMN - 1, 0 to MAX_CLUE_ROW - 1);
+		clue_columns	: 	clue_matrix_type(0 to MAX_ROW - 1, 0 to MAX_CLUE_COLUMN - 1);
+		full_cells		:	cell_array_position_type(0 to MAX_ROW * MAX_COLUMN - 1);
+		empty_cells		:	cell_array_position_type(0 to MAX_ROW * MAX_COLUMN - 1);
 	end record;
 	
-	-- constraints
-	type constraint is record
-		size			:	integer; -- TODO: Think a more meaningful term.
-		min_start	:	integer;
-		max_end		: 	integer;
-	end record;
-	type constraints_array_type is array (natural range <>, natural range <>) of constraint;
-	type constraints_board_type is record
-		rows	:	constraints_array_type(0 to (ROWS - 1), 0 to (CLUES_COLUMNS - 1));
-		cols	:	constraints_array_type(0 to (COLUMNS - 1), 0 to (CLUES_ROWS - 1));
-	end record;
+	type level_array_type is array(natural range 0 to MAX_LEVEL - 1) of level_type;
 	
-	-- queries
-	type line_enum is (ROW, COL);
-	
-	type line_query is record
-		index			:	integer range 0 to BOARD_SIZE; -- Be careful. Possible overflow here!!
-		line_type	: 	line_enum;
-	end record;
-	
-	type clues_query is record
-		index			: 	integer range 0 to BOARD_SIZE; -- Be careful. Possible overflow here!!
-		line_type	: 	line_enum;
-	end record;
-	
-	type constraints_query is record
-		index			: 	integer range 0 to BOARD_SIZE; -- Be careful. Possible overflow here!!
-		line_type	: 	line_enum;
-	end record;
-	
-	-- clues definition
-	constant INPUT_CLUES	:	clues_board_type :=
+	constant EMPTY_LEVEL : level_type :=
 	(
-		rows	=> ((5,0,0,0,0), (4,2,0,0,0), (3,4,0,0,0), (1,4,2,0,0), (7,0,0,0,0), (1,1,0,0,0), (1,1,0,0,0), (1,1,0,0,0), (1,1,0,0,0), (10,0,0,0,0)),
-		cols	=> ((1,0,0,0,0), (2,1,0,0,0), (2,1,1,0,0), (5,2,1,0,0), (2,3,2,0,0), (5,1,0,0,0), (1,4,2,0,0), (3,1,2,1,0), (4,1,0,0,0), (1,1,0,0,0))
+		rows 				=> 0,
+		columns			=> 0,
+		clue_rows		=> 
+		(
+			others => (others => -1)
+		),
+		clue_columns	=> 
+		(
+			others => (others => -1)
+		),
+		full_cells		=>
+		(
+			others => (-1, -1)
+		),
+		empty_cells		=>
+		(
+			others => (-1, -1)
+		)
 	);
-	
---===== VIRTUAL DEFINITIONS =======================================================================================================================================
-	
-	-- lines
-	type line_type is array(0 to (BOARD_SIZE - 1)) of cell; -- Be careful. Possible overflow here!!
-	type clues_type is array(0 to (CLUES_SIZE - 1)) of clue; -- Be careful. Possible overflow here!!
-	type constraints_type is array(0 to (CLUES_SIZE - 1)) of constraint; -- Be careful. Possible overflow here!!
-		
---===== FUNCTION DEFINITIONS =======================================================================================================================================
 
-	function get_line(board: board_type; query: line_query) return line_type;
-	function get_clues(clues: clues_board_type; query: clues_query) return clues_type;
-	function get_constraints(constraints: constraints_board_type; query: constraints_query) return constraints_type;
-	function get_constraints_length(constraints: constraints_type) return integer;
-	function init_constraints(clues: clues_board_type) return constraints_board_type;
-	
+	constant INPUT : level_array_type :=
+	(
+		(
+			rows 				=> 3,
+			columns			=> 3,
+			clue_rows		=> 
+			(
+				(1,1, others => -1),
+				(1, others => -1),
+				(1,1, others => -1),
+				others => (others => -1)
+			),
+			clue_columns	=> 
+			(
+				(1,1, others => -1),
+				(1, others => -1),
+				(1,1, others => -1),
+				others => (others => -1)
+			),
+			full_cells		=>
+			(
+				(0,0),
+				others => (-1, -1)
+			),
+			empty_cells		=>
+			(
+				(1,2),
+				others => (-1, -1)
+			)
+		),
+		others => EMPTY_LEVEL
+	);
 	
 end package;
 
 package body nonogram_package is
-	
-	function get_line(board: board_type; query: line_query) return line_type is
-		variable result : line_type;
-	begin
-		case (query.line_type) is
-			when ROW =>
-				for i in 0 to (ROWS - 1) loop
-					result(i) := board.cells(i, query.index);
-				end loop;
-			when COL =>
-				for i in 0 to (COLUMNS - 1) loop
-					result(i) := board.cells(query.index, i);
-				end loop;
-		end case;
-		return result;
-	end;
-	
-	function get_clues(clues: clues_board_type; query: clues_query) return clues_type is
-		variable result : clues_type;
-	begin
-  		case (query.line_type) is
-  			when ROW =>
-  				for i in 0 to (CLUES_ROWS - 1) loop
-					result(i) := clues.rows(query.index, i);
-				end loop;
-  			when COL =>
-  				for i in 0 to (CLUES_COLUMNS - 1) loop
-					result(i) := clues.cols(query.index, i);
-				end loop;
-  		end case;
-  		return result;
-	end;
-	
-	function get_constraints(constraints: constraints_board_type; query: constraints_query) return constraints_type is
-		variable result : constraints_type;
-	begin
-		case (query.line_type) is
-			when ROW =>
-				for i in 0 to (CLUES_ROWS - 1) loop
-					result(i) := constraints.rows(query.index, i);
-				end loop;
-			when COL =>
-				for i in 0 to (CLUES_COLUMNS - 1) loop
-					result(i) := constraints.cols(query.index, i);
-				end loop;
-		end case;
-		return result;
-	end;
-	
-	function get_constraints_length(constraints: constraints_type) return integer is
-		variable result : integer := 0;
-	begin
-		for i in constraints'range loop
-			if constraints(i).size > 0 then
-				result := result + 1;
-			else
-				return result;
-			end if;
-		end loop;
-	end;
-	
-	function init_constraints(clues: clues_board_type) return constraints_board_type is
-		variable result	: constraints_board_type;
-	begin
-		for i in 0 to (ROWS - 1) loop
-			for j in 0 to (CLUES_COLUMNS - 1) loop
-				result.rows(i, j).size := clues.rows(i, j);
-				result.rows(i, j).min_start := 0;
-				result.rows(i, j).max_end	:= COLUMNS - 1;
-			end loop;
-		end loop;
-		for i in 0 to (COLUMNS - 1) loop
-			for j in 0 to (CLUES_ROWS - 1) loop
-				result.cols(i, j).size := clues.cols(i, j);
-				result.cols(i, j).min_start := 0;
-				result.cols(i, j).max_end	:= ROWS - 1;
-			end loop;
-		end loop;
-		
-		return result;
-	end;
-	
+
 end package body;
